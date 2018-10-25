301cbb3e4db7f4579528359b96db62dfae5f10fa
Sergio León <panicoenlaxbox@gmail.com>
2018-10-25 09:37:05 +0200

    suppliers

--- src/app/crm/crm-routing.module.ts
+++ src/app/crm/crm-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomersListComponent } from './components/containers/customers-list/customers-list.component';
+import { SuppliersListComponent } from './components/containers/suppliers-list/suppliers-list.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: '', redirectTo: 'customers' },
-      { path: 'customers', component: CustomersListComponent }
+      { path: 'customers', component: CustomersListComponent },
+      { path: 'suppliers', component: SuppliersListComponent }
     ]
   }
 ];
