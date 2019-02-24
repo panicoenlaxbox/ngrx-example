e864730a4739e9f279c1e8386a844a313e7ec55e
Sergio León <panicoenlaxbox@gmail.com>
2019-02-24 11:22:24 +0100

    router state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { environment } from 'src/environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { reducers, metaReducers } from './core/reducers';
 import { CoreModule } from './core/core.module';
+import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
+import { CustomSerializer as CustomRouterStateSerializer, getInitialRouterReducerState } from './core/reducers/router.reducer';
 
 @NgModule({
   declarations: [
@@ -25,8 +27,9 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreModule.forRoot(reducers, { metaReducers, initialState: getInitialRouterReducerState() }),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({serializer: CustomRouterStateSerializer}),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production
